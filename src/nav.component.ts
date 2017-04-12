51fa424f6e21f82fbfad061c6a76a1008aef246d
xx4159 <emjeon93@gmail.com>
2017-04-12 17:49:34 +0900

    Add NavComponent template

--- src/nav.component.ts
+++ src/nav.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nav-component',
-  template: `<h1>Nav component</h1>`
+  template: './nav.component.html'
 })
 export class NavComponent {
+	title = 'Title';
+	menus = [{
+		title: 'menu1',
+		link: '/menu1'
+	}, {
+		title: 'menu2',
+		link: '/menu2'
+	}];
 
   constructor() {
   }
