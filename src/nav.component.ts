2070822a55c34d3c6d197e917ac6b1c9a5b42d96
xx4159 <emjeon93@gmail.com>
2017-04-12 17:55:02 +0900

    Modify NavComponent meta data template

--- src/nav.component.ts
+++ src/nav.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nav-component',
-  template: './nav.component.html'
+  templateUrl: './nav.component.html'
 })
 export class NavComponent {
 	title = 'Title';
